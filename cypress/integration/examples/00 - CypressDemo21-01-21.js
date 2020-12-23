25c4737a9cadc2af8636a721c56aa9bc57012849
HirjanTempelman <hirjan.tempelman@gmail.com>
2020-12-23 15:33:00 +0100

    Aanpasing viewport en uitzetten cy.pause

--- cypress/integration/examples/00 - CypressDemo21-01-21.js
+++ cypress/integration/examples/00 - CypressDemo21-01-21.js
@@ -7,17 +7,17 @@ context('Demo Cypress - Politie.nl', () => {
 
   // https://on.cypress.io/interacting-with-elements
 
-  it('Controle telefoon nummer = 112', () => {
+  it('tc 1 - Controle telefoon nummer = 112', () => {
     cy.get('span[class="title"]').contains('112');
     cy.get('span[class="subtitle"]').contains('0900-8844');
   })
 
-  it('Controle of zoekveld zichtbaar is', () => {
+  it('tc2 - Controle of zoekveld zichtbaar is', () => {
     cy.get('input[id="search-bar-input"]').should('be.visible');
    })
 
-  it('Zoeken naar adresgegevens van Politie Zwolle', () => {
-    cy.pause();
+  it('tc 3 -Zoeken naar adresgegevens van Politie Zwolle', () => {
+    // cy.pause();
       cy.log('Dan zoek ik naar Zwolle in het zoekveld');
     cy.get('input[id="search-bar-input"]', {timeout:10000}).type('Zwolle');
     cy.get('button[title="zoeken"]').eq(0).click();
