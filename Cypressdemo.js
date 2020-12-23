bdb8f8ccb504d5df1769e5b4546cbadfb60727ef
Boris Verlinsky <76516363+HirjanTempelman@users.noreply.github.com>
2020-12-23 10:59:51 +0100

    cypress demo

---
+++ Cypressdemo.js
@@ -0,0 +1,34 @@
+/// <reference types="cypress" />
+
+context('Demo Cypress - Politie.nl', () => {
+  beforeEach(() => {
+    cy.visit('www.politie.nl')
+  })
+
+  // https://on.cypress.io/interacting-with-elements
+
+  it('Controle telefoon nummer = 112', () => {
+    cy.get('span[class="title"]').contains('112');
+    cy.get('span[class="subtitle"]').contains('0900-8844');
+  })
+
+  it('Controle of zoekveld zichtbaar is', () => {
+    cy.get('input[id="search-bar-input"]').should('be.visible');
+   })
+
+  it('Zoeken naar adresgegevens van Politie Zwolle', () => {
+    cy.pause();
+      cy.log('Dan zoek ik naar Zwolle in het zoekveld');
+    cy.get('input[id="search-bar-input"]', {timeout:10000}).type('Zwolle');
+    cy.get('button[title="zoeken"]').eq(0).click();
+
+      cy.log('dan klik ik op zwolle')
+    cy.get('a[href="./mijn-buurt?geoquery=Zwolle&distance=5.0"]').click();
+
+      cy.log('dan controleer ik of de adres gegevens van de politiebureaus zichtbaar zijn in de pagina');
+    cy.get('body').contains('Koggelaan 8');
+    cy.get('body').contains('Verlengde Parklaan 36b');
+
+  })
+
+})
